f3c3dffad2cf598acf3b56ffbb7143bfdb4edd29
PowerShellPlanet <wojciechpietrzak@yahoo.com>
2019-03-24 19:55:05 +0100

    added help, and error handling when searching in trash

--- Public/APICore/Search/Search-GlpiToolsItems.ps1
+++ Public/APICore/Search/Search-GlpiToolsItems.ps1
@@ -2,31 +2,43 @@
 .SYNOPSIS
     Function is using GLPI Search Engine to get informations.
 .DESCRIPTION
-    Function is using GLPI Search Engine to get informations about:
-    
-    - Computer
-    - Monitor
-    - Software
-    - NetworkEquipment
-    - Peripheral
-    - Printer
-    - CartridgeItem
-    - ConsumableItem
-    - Phone
-    - Rack
-    - Enclosure
-    - Pdu
-    - User
-    - Group
-    
-    Based on his names.
+    Function Search for specific component in GLPI
+    Parameters are the names of options in GLPI
+    Remember that, names used in cmdlet coming from glpi URL, and can be hard to understand, but most of them are intuitional.
+    To get name you always have to look at the URL in GLPI, for example "http://glpi/front/computer.php" where "computer" is the name to use in parameter.
+.PARAMETER SearchFor
+    You can use this function with -SearchFor parameter.
+    Using TAB button you can choose desired option.
+.PARAMETER SearchType
+    You can use this function with -SearchType parameter.
+    Using TAB button you can choose desired option.
+.PARAMETER SearchField
+    You can use this function with -SearchField parameter.
+    This is an optional parameter, default value is 1 which is called Name in GLPI.
+    This parameter can take pipeline input, even from Get-GlpiToolsListSearchOptions cmdlet.
+.PARAMETER SearchValue
+    You can use this function with -SearchValue parameter.
+    This parameter can take pipeline input.
+    Provide value to the function, which is used to search for. 
+.PARAMETER SearchTrash
+    You can use this function with -SearchTrash parameter.
+    This is an optional switch parameter.
 .EXAMPLE
-    PS C:\> <example usage>
-    Explanation of what the example does
+    PS C:\> Search-GlpiToolsItems -SearchFor Computer -SearchType contains -SearchValue DC
+    Example will show every asset which contains value "DC" in the Name from Asset->Computers.
+.EXAMPLE
+    PS C:\> Search-GlpiToolsItems -SearchFor Computer -SearchType contains -SearchValue DC -SearchField 1 
+    Example will show every asset which contains value "DC" in the Name from Asset->Computers.
+    SearchFiled can be retrieved from Get-GlpiToolsListSearchOptions cmdlet, you can provide it throught pipeline.
+.EXAMPLE
+    PS C:\> Search-GlpiToolsItems -SearchFor Computer -SearchType contains -SearchValue DC -SearchField 1 -SearchInTrash
+    Example will show every asset which contains value "DC" in the Name from Asset->Computers.
+    SearchFiled can be retrieved from Get-GlpiToolsListSearchOptions cmdlet, you can provide it throught pipeline.
+    SearchInTrash will allow you to search for assets from trash.    
 .INPUTS
-    Inputs (if any)
+    Only for -SearchValue, and -SearchField.
 .OUTPUTS
-    Output (if any)
+    Function returns PSCustomObject with property's of Search results from GLPI
 .NOTES
     PSP 02/2019
 #>
@@ -226,7 +238,6 @@ function Search-GlpiToolsItems {
     
     begin {
         $SearchArray = @()
-        $IntermediateArray = @()
 
         $AppToken = $Script:AppToken
         $PathToGlpi = $Script:PathToGlpi
@@ -259,8 +270,13 @@ function Search-GlpiToolsItems {
         }
             
         $SearchResult = Invoke-RestMethod @params
-        $SearchResults = $SearchResult | Select-Object -ExpandProperty data
+        
+        try {
+            $SearchResults = $SearchResult | Select-Object -ExpandProperty data -ErrorAction Stop
+        } catch {
 
+        }
+        
         foreach ($SearchItem in $SearchResults) {
             $SearchHash = [ordered]@{}
             $DataResult = $SearchItem.PSObject.Properties | Select-Object -Property Name, Value 
@@ -281,11 +297,11 @@ function Search-GlpiToolsItems {
             }
 
             $object = [pscustomobject]$SearchHash
-            $IntermediateArray += $object 
+            $SearchArray += $object 
         }
 
-        $IntermediateArray
-        $IntermediateArray = @()
+        $SearchArray
+        $SearchArray = @()
     }
     
     end {
