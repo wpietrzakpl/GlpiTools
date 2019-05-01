3001af8163427eaf01c98c55b69de6c44dc6bc3d
PowerShellPlanet <wojciechpietrzak@yahoo.com>
2019-05-01 10:07:00 +0200

    modified help, added more examples

--- Public/Assets/Computers/Get-GlpiToolsComputers.ps1
+++ Public/Assets/Computers/Get-GlpiToolsComputers.ps1
@@ -13,7 +13,6 @@
     Parameter which you can use with ComputerId Parameter.
     ComputerId has converted parameters from default, parameter Raw allows not convert this parameters.
 .PARAMETER ComputerName
-    This parameter can take pipline input, either, you can use this function with -ComputerName keyword.
     Provide to this param Computer Name from GLPI Computers Bookmark
 .PARAMETER SearchInTrash
     Parameter which you can use with ComputerName Parameter.
@@ -22,17 +21,30 @@
     Parameter which you can use with ComputerId Parameter. 
     If you want to get additional parameter of computer object like, disks, or logs, use this parameter.
 .EXAMPLE
-    PS C:\Users\Wojtek> 326 | Get-GlpiToolsComputers
-    Function gets ComputerId from GLPI from Pipline, and return Computer object
+    PS C:\> 326 | Get-GlpiToolsComputers
+    Function gets ComputerID from GLPI from Pipline, and return Computer object
 .EXAMPLE
-    PS C:\Users\Wojtek> 326, 321 | Get-GlpiToolsComputers
-    Function gets ComputerId from GLPI from Pipline (u can pass many ID's like that), and return Computer object
+    PS C:\> 326, 321 | Get-GlpiToolsComputers
+    Function gets ComputerID from GLPI from Pipline (u can pass many ID's like that), and return Computer object
 .EXAMPLE
-    PS C:\Users\Wojtek> Get-GlpiToolsComputers -ComputerId 326
-    Function gets ComputerId from GLPI which is provided through -ComputerId after Function type, and return Computer object
+    PS C:\> Get-GlpiToolsComputers -ComputerId 326
+    Function gets ComputerID from GLPI which is provided through -ComputerId after Function type, and return Computer object
 .EXAMPLE 
-    PS C:\Users\Wojtek> Get-GlpiToolsComputers -ComputerId 326, 321
-    Function gets ComputerId from GLPI which is provided through -ComputerId keyword after Function type (u can provide many ID's like that), and return Computer object
+    PS C:\> Get-GlpiToolsComputers -ComputerId 326, 321
+    Function gets ComputerID from GLPI which is provided through -ComputerId keyword after Function type (u can provide many ID's like that), and return Computer object
+.EXAMPLE
+    PS C:\> Get-GlpiToolsComputers -ComputerId 234 -Raw
+    Example will show Computer with id 234, but without any parameter converted
+.EXAMPLE
+    PS C:\> 234 | Get-GlpiToolsComputers -Raw
+    Example will show Computer with id 234, but without any parameter converted
+.EXAMPLE
+    PS C:\> Get-GlpiToolsComputers -ComputerName glpi
+    Example will return glpi Computer, but what is the most important, Computer will be shown exacly as you see in glpi Computers tab.
+    If you want to add parameter, you have to modify "default items to show". This is the "key/tool" icon near search.
+.EXAMPLE
+    PS C:\> Get-GlpiToolsComputers -ComputerName glpi -SearchInTrash Yes
+    Example will return glpi Computer, but from trash
 .INPUTS
     Computer ID which you can find in GLPI, or use this Function to convert ID returned from other Functions
 .OUTPUTS
