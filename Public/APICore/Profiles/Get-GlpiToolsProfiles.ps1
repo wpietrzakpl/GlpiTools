72aab0df5bbdfd05c0a77ac93a4d8109c89eb60c
PowerShellPlanet <wojciechpietrzak@yahoo.com>
2019-05-01 11:23:39 +0200

    delete parameters which will not work

--- Public/APICore/Profiles/Get-GlpiToolsProfiles.ps1
+++ Public/APICore/Profiles/Get-GlpiToolsProfiles.ps1
@@ -6,30 +6,15 @@
     Returns object with property's of Profile
 .PARAMETER All
     This parameter will return all Profiles from GLPI
-.PARAMETER ProfileId
-    This parameter can take pipline input, either, you can use this function with -ProfileId keyword.
-    Provide to this param Profile ID from GLPI Profiles Bookmark
 .PARAMETER ProfileName
     This parameter can take pipline input, either, you can use this function with -ProfileName keyword.
     Provide to this param Profile Name from GLPI Profiles Bookmark
-.PARAMETER SearchInTrash
-    Parameter which you can use with ProfileName Parameter.
-    If you want Search for Profile name in trash, that parameter allow you to do it.
-.PARAMETER Parameter
-    Parameter which you can use with ProfileId Parameter. 
-    If you want to get additional parameter of Profile object like, disks, or logs, use this parameter.
 .EXAMPLE
-    PS C:\Users\Wojtek> 326 | Get-GlpiToolsProfiles
-    Function gets ProfileId from GLPI from Pipline, and return Profile object
+    PS C:\> Get-GlpiToolsProfiles -All
+    Example will show all profiles.
 .EXAMPLE
-    PS C:\Users\Wojtek> 326, 321 | Get-GlpiToolsProfiles
-    Function gets ProfileId from GLPI from Pipline (u can pass many ID's like that), and return Profile object
-.EXAMPLE
-    PS C:\Users\Wojtek> Get-GlpiToolsProfiles -ProfileId 326
-    Function gets ProfileId from GLPI which is provided through -ProfileId after Function type, and return Profile object
-.EXAMPLE 
-    PS C:\Users\Wojtek> Get-GlpiToolsProfiles -ProfileId 326, 321
-    Function gets ProfileId from GLPI which is provided through -ProfileId keyword after Function type (u can provide many ID's like that), and return Profile object
+    PS C:\> Get-GlpiToolsProfiles -ProfileName Admin
+    Example will return every profile which contains Admin keyword
 .INPUTS
     Profile ID which you can find in GLPI, or use this Function to convert ID returned from other Functions
 .OUTPUTS
@@ -47,20 +32,9 @@ function Get-GlpiToolsProfiles {
         [switch]$All,
 
         [parameter(Mandatory = $true,
-            ValueFromPipeline = $true,
-            ParameterSetName = "ProfileId")]
-        [alias('PID')]
-        [int[]]$ProfileId,
-
-        [parameter(Mandatory = $true,
             ParameterSetName = "ProfileName")]
         [alias('PN')]
-        [string]$ProfileName,
-        [parameter(Mandatory = $false,
-            ParameterSetName = "ProfileName")]
-        [alias('SIT')]
-        [ValidateSet("Yes", "No")]
-        [string]$SearchInTrash = "No"
+        [string]$ProfileName
 
     )
     
@@ -108,42 +82,8 @@ function Get-GlpiToolsProfiles {
                 $ProfileObjectArray
                 $ProfileObjectArray = @()
             }
-            ProfileId { 
-                foreach ( $CId in $ProfileId ) {
-                    $params = @{
-                        headers = @{
-                            'Content-Type'  = 'application/json'
-                            'App-Token'     = $AppToken
-                            'Session-Token' = $SessionToken
-                        }
-                        method  = 'get'
-                        uri     = "$($PathToGlpi)/Profile/$($CId)$ParamValue"
-                    }
-
-                    Try {
-                        $GlpiProfile = Invoke-RestMethod @params -ErrorAction Stop
-
-                        $ProfileHash = [ordered]@{ }
-                        $ProfileProperties = $GlpiProfile.PSObject.Properties | Select-Object -Property Name, Value 
-                                
-                        foreach ($ProfileProp in $ProfileProperties) {
-                            $ProfileHash.Add($ProfileProp.Name, $ProfileProp.Value)
-                        }
-                        $object = [pscustomobject]$ProfileHash
-                        $ProfileObjectArray += $object 
-
-                    }
-                    Catch {
-
-                        Write-Verbose -Message "Profile ID = $CId is not found"
-                        
-                    }
-                    $ProfileObjectArray
-                    $ProfileObjectArray = @()
-                }
-            }
             ProfileName { 
-                Search-GlpiToolsItems -SearchFor Profile -SearchType contains -SearchValue $ProfileName -SearchInTrash $SearchInTrash
+                Search-GlpiToolsItems -SearchFor Profile -SearchType contains -SearchValue $ProfileName
             }
             Default {
                 
