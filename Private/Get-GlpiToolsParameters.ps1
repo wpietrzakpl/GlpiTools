f74bd11b3ef5072b02b13a4768f3f7d5f73f36cc
PowerShellPlanet <wojciechpietrzak@yahoo.com>
2019-10-30 20:41:38 +0100

    increase speed

--- Private/Get-GlpiToolsParameters.ps1
+++ Private/Get-GlpiToolsParameters.ps1
@@ -30,24 +30,17 @@ function Get-GlpiToolsParameters {
     )
     
     begin {
-        $AppToken = $Script:AppToken
-        $PathToGlpi = $Script:PathToGlpi
-        $SessionToken = $Script:SessionToken
 
-        $AppToken = Get-GlpiToolsConfig -Verbose:$false | Select-Object -ExpandProperty AppToken
-        $PathToGlpi = Get-GlpiToolsConfig -Verbose:$false | Select-Object -ExpandProperty PathToGlpi
-        $SessionToken = Set-GlpiToolsInitSession -Verbose:$false | Select-Object -ExpandProperty SessionToken
     }
     
     process { 
-         
         try {
-            if  ($Parameter -eq "entities_id") {
-                $ConvertedValue = $Value | Get-GlpiToolsEntities | Select-Object -ExpandProperty CompleteName -ErrorAction Stop
+            if ($Parameter -eq "entities_id") {
+                $ConvertedValue = $Value | Get-GlpiToolsEntities -Raw | Select-Object -ExpandProperty CompleteName -ErrorAction Stop
             } elseif ($Parameter -eq "users_id_tech" ) {
-                $ConvertedValue = $Value | Get-GlpiToolsUsers | Select-Object realname, firstname | ForEach-Object { "{0} {1}" -f $_.firstname,$_.realname } -ErrorAction Stop
+                $ConvertedValue = $Value | Get-GlpiToolsUsers -Raw | Select-Object realname, firstname | ForEach-Object { "{0} {1}" -f $_.firstname,$_.realname } -ErrorAction Stop
             } elseif ($Parameter -eq "groups_id_tech" ) {
-                $ConvertedValue = $Value | Get-GlpiToolsGroups | Select-Object -ExpandProperty name -ErrorAction Stop
+                $ConvertedValue = $Value | Get-GlpiToolsGroups -Raw | Select-Object -ExpandProperty name -ErrorAction Stop
             } elseif ($Parameter -eq "autoupdatesystems_id"  ) {
                 $ConvertedValue = $Value | Get-GlpiToolsDropdownsOSUpdateSources | Select-Object -ExpandProperty name -ErrorAction Stop
             } elseif ( $Parameter -eq "locations_id"  ) {
@@ -56,37 +49,33 @@ function Get-GlpiToolsParameters {
                 $ConvertedValue = $Value | Get-GlpiToolsDropdownsDomains | Select-Object -ExpandProperty name -ErrorAction Stop
             } elseif ($Parameter -eq "networks_id" ) {
                 $ConvertedValue = $Value | Get-GlpiToolsDropdownsNetworks | Select-Object -ExpandProperty name -ErrorAction Stop
-            } elseif ($Parameters -eq "computermodels_id" ) {
-                $ConvertedValue = $Value | Get-GlpiToolsDropdownsComputerModels | Select-Object -ExpandProperty name -ErrorAction Stop
-            } elseif ($Parameters -eq "computertypes_id" ) {
-                $ConvertedValue = $Value | Get-GlpiToolsDropdownsComputerTypes | Select-Object -ExpandProperty name -ErrorAction Stop
+            } elseif ($Parameter -eq "computermodels_id" ) {
+                $ConvertedValue = $Value | Get-GlpiToolsDropdownsComputerModels -Raw | Select-Object -ExpandProperty name -ErrorAction Stop
+            } elseif ($Parameter -eq "computertypes_id" ) {
+                $ConvertedValue = $Value | Get-GlpiToolsDropdownsComputerTypes -Raw | Select-Object -ExpandProperty name -ErrorAction Stop
             } elseif ($Parameter -eq "manufacturers_id" ) {
                 $ConvertedValue = $Value | Get-GlpiToolsDropdownsManufacturers | Select-Object -ExpandProperty name -ErrorAction Stop
             } elseif ($Parameter -eq "users_id" ) {
-                $ConvertedValue = $Value | Get-GlpiToolsUsers | Select-Object realname, firstname | ForEach-Object { "{0} {1}" -f $_.firstname,$_.realname } -ErrorAction Stop
+                $ConvertedValue = $Value | Get-GlpiToolsUsers -Raw | Select-Object realname, firstname | ForEach-Object { "{0} {1}" -f $_.firstname,$_.realname } -ErrorAction Stop
             } elseif ($Parameter -eq "groups_id" ) {
-                $ConvertedValue = $Value | Get-GlpiToolsGroups | Select-Object -ExpandProperty name -ErrorAction Stop
+                $ConvertedValue = $Value | Get-GlpiToolsGroups -Raw | Select-Object -ExpandProperty name -ErrorAction Stop
             } elseif ($Parameter -eq "states_id" ) {
                 $ConvertedValue = $Value | Get-GlpiToolsDropdownsStatusesOfItems | Select-Object -ExpandProperty name -ErrorAction Stop 
             } elseif ($Parameter -eq "softwares_id") {
-                $ConvertedValue = $Value | Get-GlpiToolsSoftware | Select-Object -ExpandProperty name -ErrorAction Stop 
+                $ConvertedValue = $Value | Get-GlpiToolsSoftware -Raw | Select-Object -ExpandProperty name -ErrorAction Stop 
             } elseif ($Parameter -eq "softwareversions_id") {
-                $ConvertedValue = $Value | Get-GlpiToolsSoftwareVersions | Select-Object -ExpandProperty name -ErrorAction Stop 
+                $ConvertedValue = $Value | Get-GlpiToolsSoftwareVersions -Raw | Select-Object -ExpandProperty name -ErrorAction Stop 
             } elseif ($Parameter -eq "computers_id") {
-                $ConvertedValue = $Value | Get-GlpiToolsComputers | Select-Object -ExpandProperty name -ErrorAction Stop 
-            } 
-            else {
+                $ConvertedValue = $Value | Get-GlpiToolsComputers -Raw | Select-Object -ExpandProperty name -ErrorAction Stop 
+            } else {
                 $ConvertedValue = $Value
             }
         } catch {
-            $ConvertedValue = 0
+            $ConvertedValue = "Blank"
         }
-        
-        
     }
     
     end {
         $ConvertedValue
-        Set-GlpiToolsKillSession -SessionToken $SessionToken -Verbose:$false
     }
 }
